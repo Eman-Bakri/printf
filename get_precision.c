641392f84c2ad6ca82ed68d483febea24242dde2
nouran1234567 <nouran.a.mohamed23@gmail.com>
2023-07-24 18:07:11 -0700

    get_flags.c

--- get_precision.c
+++ get_precision.c
@@ -10,24 +10,24 @@
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	int dec_i = *i + 1;
+	int curr_i = *i + 1;
 	int precision = -1;
 
-	if (format[dec_i] != '.')
+	if (format[curr_i] != '.')
 		return (precision);
 
 	precision = 0;
 
-	for (dec_i += 1; format[dec_i] != '\0'; dec_i++)
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (is_digit(format[dec_i]))
+		if (is_digit(format[curr_i]))
 		{
 			precision *= 10;
-			precision += format[dec_i] - '0';
+			precision += format[curr_i] - '0';
 		}
-		else if (format[dec_i] == '*')
+		else if (format[curr_i] == '*')
 		{
-			dec_i++;
+			curr_i++;
 			precision = va_arg(list, int);
 			break;
 		}
@@ -35,7 +35,7 @@ int get_precision(const char *format, int *i, va_list list)
 			break;
 	}
 
-	*i = dec_i - 1;
+	*i = curr_i - 1;
 
 	return (precision);
 }
